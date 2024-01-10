b5b14d81adf9e7135970f3acefb6b4834012d87b
Maria Julia Fernandez Balice (MJ) <136009647+Mjuliafb@users.noreply.github.com>
2024-01-10 20:57:45 +0000

    first commit pending alignment

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	const OpacidadBoton = () => {
+		const [opacidadBoton, setOpacidadBoton] = useState(0.4);
+		const [opacidadBoton2, setOpacidadBoton2] = useState(0.4);
+		const [opacidadBoton3, setOpacidadBoton3] = useState(0.4);
+
+		const cambiarOpacidad = () => {
+			setOpacidadBoton(opacidadBoton === 0.4 ? 1 : 0.4);
+		};
+
+		const cambiarOpacidad2 = () => {
+			setOpacidadBoton2(opacidadBoton2 === 0.4 ? 1 : 0.4);
+		};
+
+		const cambiarOpacidad3 = () => {
+			setOpacidadBoton3(opacidadBoton3 === 0.4 ? 1 : 0.4);
+		};
+
+		return (
+			<div className="container text-center">
+				<div className="backButton">
+					<button
+						className="yellow"
+						style={{ opacity: opacidadBoton, transition: "opacity 0.5s" }}
+						onClick={cambiarOpacidad}
+					></button>
+				</div>
+				<div className="backButton">
+					<button
+						className="blue"
+						style={{ opacity: opacidadBoton2, transition: "opacity 0.5s" }}
+						onClick={cambiarOpacidad2}
+					></button>
+				</div>
+				<div className="backButton">
+					<button
+						className="red"
+						style={{ opacity: opacidadBoton3, transition: "opacity 0.5s" }}
+						onClick={cambiarOpacidad3}
+					></button>
+				</div>
+			</div>
+		);
+	};
+
+	return <OpacidadBoton />;
 };
 
 export default Home;
